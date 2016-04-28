3125c46f37e986ab5231dc965f48bee7dbd8bb2a
Jesus <jesus.iniguezgarcia@alum.uca.es>
2016-04-28 16:47:15 +0200

    Comentarios y ridmi

--- pb.c
+++ pb.c
@@ -28,52 +28,31 @@ int calculate_result(int white_balls[5], int power_ball)
 {
   for (int i=0; i<5; i++)
     {
-      if ((white_balls[i] < 1) || (white_balls[i] > MAX_WHITE_BALL))
-	{
-	  return -1;
-	}
+      	if ((white_balls[i] < 1) || (white_balls[i] > MAX_WHITE_BALL))
+		{
+		 	return -1;
+		}
     }
-  qsort (white_balls, 5, sizeof(int), my_sort_func();
-  return 0;
+
+    // lottery ball numbers are always shown sorted
+  	qsort (white_balls, 5, sizeof(int), my_sort_func();
+  	return 0;
 }
 int main(int argc, char** argv)
 {
 	int balls[6];
 	int count_balls = 0;
-	int favorite = 0;
+	int favorite = 0;// this should be a bool
+
 	for (int i=1; i<argc; i++)
-	{
-		const char* arg = argv[i];
-		if ('-' == arg[0])
-		{
-			if (0 == strcmp(arg, "-favorite"))
-			{
-				favorite = 1;
-			}
-			else
-			{
-				goto usage_error;
-			}
-		}
-		else
-		{
-			char* endptr = NULL;
-			long val = strtol(arg, &endptr, 10);
-			if (*endptr)
-			{
-				goto usage_error;
-			}
-			balls[count_balls++] = (int) val;
-		}
-	}
-	
-	if (6 != count_balls)
 	{
 		goto usage_error;
 	}
+	// the power ball is always the last one given
 	int power_ball = balls[5];
 	int result = calculate_result(balls, power_ball);
-	
+	// calculate result can return -1 if the ball numbers
+	// are out of range
 	if (result < 0)
 	{
 		goto usage_error;
